845e1a8aec71497a44680c7897197ae8b6da7103
Aditya Kurniawan <akurniawan.cs@gmail.com>
2026-08-31 19:08:10 +0400

    Add: longest-consecutive-sequence - submission-6

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-6.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        if (nums.size() == 0) return 0;
+        else if (nums.size() == 1) return 1;
+
+        std::sort(nums.begin(), nums.end());
+        int last_num = nums[0];
+        int counter = 1;
+        int res = 0;
+
+        for (int i = 1; i < nums.size(); ++i) {
+            const int& num = nums[i];
+            if (num - last_num != 1) {
+                // std::cout << "enter a" << std::endl;
+                if (num - last_num > 1) counter = 1;
+                last_num = num;
+                continue;
+            } else {
+                // std::cout << "enter b" << std::endl;
+                last_num = num;
+                counter += 1;
+                res = max(res, counter);
+            }
+            // std::cout << num << " " << res << " " << counter << std::endl;
+        }
+
+        return max(res, counter);
+    }
+};
